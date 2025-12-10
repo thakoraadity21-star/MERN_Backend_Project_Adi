53696c243ff16e36c20ff7905c43c272e4578eb8
Adi Thakor <thakoraadity21@gmail.com>
2025-12-10 14:40:46 +0530

    Fix: Converted userModel to ES Module export

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,26 +1,39 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-//define schema for the user
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true, 
-        // step1
+const UserSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please add a name'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Please add an email'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Please add a password'],
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },//step2
-    password: {
-        type: String,
-        required: true,
-        //step3 
-    },
-}, {
-    timestamps: true,
+    {
+        timestamps: true,
+    }
+);
+
+// Mongoose Pre-Save Hook (Optional, but good practice)
+// पासवर्ड को सेव करने से पहले हैश करने के लिए
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+const User = mongoose.model('User', UserSchema);
+
+// ES Module एक्सपोर्ट
+export default User;
